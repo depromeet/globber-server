ca78f2e47d110d25073da8c81b55f5214cd08c58
Minu Kim <kmw106933@naver.com>
2025-09-04 17:01:27 +0900

    initial commit

---
+++ src/main/java/backend/globber/GlobberApplication.java
@@ -0,0 +1,13 @@
+package backend.globber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobberApplication.class, args);
+    }
+
+}
