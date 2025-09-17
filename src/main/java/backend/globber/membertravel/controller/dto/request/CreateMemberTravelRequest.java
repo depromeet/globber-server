c11e430af482ffa19ab064e375049f909d4935ae
hyn <qqqq_o@naver.com>
2025-09-18 00:29:32 +0900

    refactor: CityCoordinates, CreateMemberTravelRequest record로 변경

--- src/main/java/backend/globber/membertravel/controller/dto/request/CreateMemberTravelRequest.java
+++ src/main/java/backend/globber/membertravel/controller/dto/request/CreateMemberTravelRequest.java
@@ -2,22 +2,20 @@ package backend.globber.membertravel.controller.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-public class CreateMemberTravelRequest {
+public record CreateMemberTravelRequest(
 
-  @NotBlank(message = "국가명은 필수입니다")
-  private String countryName;
+    @NotBlank(message = "국가명은 필수입니다")
+    String countryName,
 
-  @NotBlank(message = "도시명은 필수입니다")
-  private String cityName;
+    @NotBlank(message = "도시명은 필수입니다")
+    String cityName,
 
-  @NotNull(message = "위도는 필수입니다")
-  private Double lat;
+    @NotNull(message = "위도는 필수입니다")
+    Double lat,
 
-  @NotNull(message = "경도는 필수입니다")
-  private Double lng;
+    @NotNull(message = "경도는 필수입니다")
+    Double lng
+) {
+
 }
