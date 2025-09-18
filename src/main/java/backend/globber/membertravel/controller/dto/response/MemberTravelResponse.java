dfa89f5c9d7aa16a615dc318d7dddf28fda297a8
hyn <qqqq_o@naver.com>
2025-09-19 00:25:00 +0900

    refactor: redis 설정 및 관련 코드 제거, GET/POST 껍데기만 남기기

--- src/main/java/backend/globber/membertravel/controller/dto/response/MemberTravelResponse.java
+++ src/main/java/backend/globber/membertravel/controller/dto/response/MemberTravelResponse.java
@@ -3,11 +3,10 @@ package backend.globber.membertravel.controller.dto.response;
 import backend.globber.membertravel.controller.dto.CountryInfo;
 import java.util.List;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-public class MemberTravelResponse {
+public record MemberTravelResponse(
+    List<CountryInfo> countries
+) {
 
-  private List<CountryInfo> countries;
 }
