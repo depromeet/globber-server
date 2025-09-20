d41c819dbcb48368ec2aa5a63eff5b79d8cc9166
Minu Kim <kmw106933@naver.com>
2025-09-20 22:16:29 +0900

    refactor: test 수정

--- src/test/java/backend/globber/GlobberApplicationTests.java
+++ src/test/java/backend/globber/GlobberApplicationTests.java
@@ -1,13 +1,10 @@
 package backend.globber;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GlobberApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class GlobberApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
