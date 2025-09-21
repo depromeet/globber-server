1864f8f2eeb1bf23aa0060bb23e2db0c6f0e4b22
Minu Kim <kmw106933@naver.com>
2025-09-21 20:58:38 +0900

    feat: 검색어 조회 API 추가

--- src/main/java/backend/globber/GlobberApplication.java
+++ src/main/java/backend/globber/GlobberApplication.java
@@ -2,8 +2,10 @@ package backend.globber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories(basePackages = "backend.globber.city.repository")
 public class GlobberApplication {
 
     public static void main(String[] args) {
