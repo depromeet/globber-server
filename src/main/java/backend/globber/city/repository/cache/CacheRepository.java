ffc0c8ebf29bbfa2d6ecc11564f11a357be2fb52
Minu Kim <kmw106933@naver.com>
2025-09-21 20:58:16 +0900

    feat: 캐시 추가

---
+++ src/main/java/backend/globber/city/repository/cache/CacheRepository.java
@@ -0,0 +1,27 @@
+package backend.globber.city.repository.cache;
+
+import backend.globber.city.controller.dto.SearchResult;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+@Repository
+public class CacheRepository {
+
+    private final RedisTemplate<String, SearchResult> redisTemplate;
+
+    public CacheRepository(
+            @Qualifier("redisTemplate2") RedisTemplate<String, SearchResult> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public SearchResult get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void set(String key, SearchResult value, int ttlMinutes) {
+        redisTemplate.opsForValue().set(key, value, ttlMinutes, TimeUnit.MINUTES);
+    }
+}
