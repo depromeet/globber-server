edc3759536d3d47e4222726293c493645b9f189f
Minu Kim <kmw106933@naver.com>
2025-09-21 20:54:04 +0900

    refactor: 테스트 컨테이너 설정 변경

--- src/test/java/backend/globber/support/RedisTestConfig.java
+++ src/test/java/backend/globber/support/RedisTestConfig.java
@@ -2,8 +2,10 @@ package backend.globber.support;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
@@ -27,6 +29,7 @@ public class RedisTestConfig {
     }
 
     @Bean
+    @Primary
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(
                 redisContainer.getHost(),
