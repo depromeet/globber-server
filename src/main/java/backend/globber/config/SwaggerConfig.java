c4bbc0a7668888f2255f47647d031350fa9ed4a0
Minu Kim <kmw106933@naver.com>
2025-09-23 21:12:23 +0900

    refactor: 스웨거 cors 변경

--- src/main/java/backend/globber/config/SwaggerConfig.java
+++ src/main/java/backend/globber/config/SwaggerConfig.java
@@ -1,22 +1,27 @@
 package backend.globber.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https:/globber.store", description = "운영 서버"),
+                @Server(url = "http://localhost:8080", description = "로컬 서버")
+        })
 public class SwaggerConfig {
 
     @Bean
     // 명세서 생성
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .info(new Info()
-                .title("GLOBBER")
-                .version("1.0")
-                .description("GLOBBER API 명세서입니다."));
+                .info(new Info()
+                        .title("GLOBBER")
+                        .version("1.0")
+                        .description("GLOBBER API 명세서입니다."));
     }
 
     @Bean
@@ -25,9 +30,9 @@ public class SwaggerConfig {
         String[] paths = {"/**"};
         String[] packagesToScan = {"backend.globber"};
         return GroupedOpenApi.builder()
-            .group("GLOBBER API")
-            .pathsToMatch(paths)
-            .packagesToScan(packagesToScan)
-            .build();
+                .group("GLOBBER API")
+                .pathsToMatch(paths)
+                .packagesToScan(packagesToScan)
+                .build();
     }
 }
