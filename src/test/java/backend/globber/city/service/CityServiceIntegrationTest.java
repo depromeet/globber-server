661ba499c1cf17f11bc4a241582cdd1fafef2d38
Minu Kim <kmw106933@naver.com>
2025-09-25 03:21:05 +0900

    [FEAT] 응답 형식에 나라 코드 추가 (#27)
    * feat: 응답 형식에 나라 코드 추가
    
    * refactor: 유니크 제약 조건 추가
    
    * test: 테스트 코드 수정

--- src/test/java/backend/globber/city/service/CityServiceIntegrationTest.java
+++ src/test/java/backend/globber/city/service/CityServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package backend.globber.city.service;
 
+import backend.globber.city.controller.dto.CityResponse;
 import backend.globber.city.controller.dto.RecommendResponse;
 import backend.globber.city.domain.City;
 import backend.globber.city.repository.CityRepository;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -21,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ContextConfiguration(initializers = RedisTestConfig.Initializer.class)
 @Import({PostgresTestConfig.class, RedisTestConfig.class})
+@Transactional
 class CityServiceIntegrationTest {
 
     @Autowired
@@ -36,9 +39,7 @@ class CityServiceIntegrationTest {
     @Test
     @DisplayName("인기 도시 조회 시 limit 개수만큼 반환된다")
     void getTopCities_limitTest() {
-        List<City> cities = IntStream.rangeClosed(1, 50)
-                .mapToObj(i -> new City(null, "City" + i, "Country" + i, 123.12, 123.12, "KOR"))
-                .toList();
+        List<City> cities = IntStream.rangeClosed(1, 50).mapToObj(i -> new City(null, "City" + i, "Country" + i, 123.12, 123.12, "KOR")).toList();
         cityRepository.saveAll(cities);
         cities.forEach(rankingRepository::incrementScore);
 
@@ -53,9 +54,7 @@ class CityServiceIntegrationTest {
     @Test
     @DisplayName("Redis에 데이터가 없으면 DB에서 limit 개수 조회한다")
     void getTopCities_fallbackToDb() {
-        List<City> cities = IntStream.rangeClosed(1, 50)
-                .mapToObj(i -> new City(null, "City" + i, "Country" + i, 123.12, 123.12, "KOR"))
-                .toList();
+        List<City> cities = IntStream.rangeClosed(1, 50).mapToObj(i -> new City(null, "City" + i, "Country" + i, 123.12, 123.12, "KOR")).toList();
         cityRepository.saveAll(cities);
 
         int limit = 5;
@@ -64,4 +63,33 @@ class CityServiceIntegrationTest {
 
         assertThat(response.cityResponseList()).hasSize(limit);
     }
+
+    @Test
+    @DisplayName("Redis에 데이터가 없으면 DB에서 limit 개수 조회한다 (모든 필드 검증)")
+    void getTopCities_fallbackToDb_fullFields() {
+        // given
+        List<City> cities = IntStream.rangeClosed(1, 5).mapToObj(i -> City.builder().cityId(null).cityName("City" + i).countryName("Country" + i).lat(123.12).lng(456.78).countryCode("KOR").build()).toList();
+        cityRepository.saveAll(cities);
+
+
+        int limit = 5;
+
+        // when
+        RecommendResponse response = cityService.getTopCities(limit);
+
+        // then
+        assertThat(response.cityResponseList()).hasSize(limit);
+
+        var expectedNames = cities.stream().map(City::getCityName).toList();
+        var expectedCountries = cities.stream().map(City::getCountryName).toList();
+
+        assertThat(response.cityResponseList().stream().map(CityResponse::cityName)).containsExactlyInAnyOrderElementsOf(expectedNames);
+        assertThat(response.cityResponseList().stream().map(CityResponse::countryName)).containsExactlyInAnyOrderElementsOf(expectedCountries);
+        assertThat(response.cityResponseList()).allSatisfy(dto -> {
+            assertThat(dto.lat()).isEqualTo(123.12);
+            assertThat(dto.lng()).isEqualTo(456.78);
+            assertThat(dto.countryCode()).isEqualTo("KOR");
+        });
+    }
+
 }
