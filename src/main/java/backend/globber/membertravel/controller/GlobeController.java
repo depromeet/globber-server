b5bfbc9a12a807440c9c64c39b6ddac78a33959e
Minu Kim <kmw106933@naver.com>
2025-09-25 10:20:33 +0900

    refactor: 네이밍 변경

--- src/main/java/backend/globber/membertravel/controller/GlobeController.java
+++ src/main/java/backend/globber/membertravel/controller/GlobeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/globes")
+@RequestMapping("api/v1/globes")
 public class GlobeController {
 
     private final GlobeService globeService;
