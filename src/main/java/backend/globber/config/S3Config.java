939da1b954085f5d810ed3f5fa410e95bef8f8f8
hyn <qqqq_o@naver.com>
2025-10-06 18:32:41 +0900

    refactor: [P2] 프로덕션 환경에서는 IAM Role 기반 인증 고려 반영

--- src/main/java/backend/globber/config/S3Config.java
+++ src/main/java/backend/globber/config/S3Config.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -25,9 +27,13 @@ public class S3Config {
     public S3Client s3Client() {
         return S3Client.builder()
             .region(Region.of(region))
-            .credentialsProvider(StaticCredentialsProvider.create(
-                AwsBasicCredentials.create(accessKey, secretKey)
-            ))
+            .credentialsProvider(
+                AwsCredentialsProviderChain.builder()
+                    .addCredentialsProvider(DefaultCredentialsProvider.create())  // 1순위: IAM Role
+                    .addCredentialsProvider(StaticCredentialsProvider.create(     // 2순위: 환경변수
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                    .build()
+            )
             .build();
     }
 
