59eb9bd2382f143f90c529c79dc513a267335433
hyn <qqqq_o@naver.com>
2025-10-26 17:19:08 +0900

    refactor: 북마크 조회 시 N+1 발생으로 인한 fetch join 추가

--- src/main/java/backend/globber/bookmark/repository/BookmarkRepository.java
+++ src/main/java/backend/globber/bookmark/repository/BookmarkRepository.java
@@ -4,11 +4,24 @@ import backend.globber.bookmark.domain.Bookmark;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
+    @Query("""
+        SELECT b FROM Bookmark b
+        JOIN FETCH b.targetMember
+        WHERE b.member.id = :memberId
+        ORDER BY b.createdAt DESC
+        """)
     List<Bookmark> findAllByMember_IdOrderByCreatedAtDesc(Long memberId);
 
+    @Query("""
+        SELECT b FROM Bookmark b
+        JOIN FETCH b.targetMember
+        WHERE b.member.id = :memberId
+        ORDER BY b.targetMember.name ASC
+        """)
     List<Bookmark> findAllByMember_IdOrderByTargetMember_NameAsc(Long memberId);
 
     @Modifying
