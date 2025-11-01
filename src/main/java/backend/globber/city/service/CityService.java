9b7df932023a49b735cb2ad42b4a3fc3419b4119
MINSEOK LEE <91869302+minseokey@users.noreply.github.com>
2025-11-01 12:05:18 +0900

    [FEATURE] 도시편집기능 개발 (#74)
    * feat: 도시추가, 도시수정, 도시삭제 로직 추가.
    
    * Delete log directory
    
    * feat: gitignore에 ignore 추가
    
    * feat: 수정사항 반영

--- src/main/java/backend/globber/city/service/CityService.java
+++ src/main/java/backend/globber/city/service/CityService.java
@@ -1,15 +1,22 @@
 package backend.globber.city.service;
 
+import backend.globber.auth.domain.Member;
+import backend.globber.auth.repository.MemberRepository;
+import backend.globber.city.controller.dto.CityRequest;
+import backend.globber.city.controller.dto.CityResponse;
 import backend.globber.city.controller.dto.CityUniqueDto;
 import backend.globber.city.controller.dto.RecommendResponse;
 import backend.globber.city.domain.City;
 import backend.globber.city.repository.CityRepository;
 import backend.globber.city.repository.cache.RankingRepository;
+import backend.globber.exception.spec.CityNotFoundException;
+import backend.globber.exception.spec.UsernameNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +24,7 @@ public class CityService {
 
     private final CityRepository cityRepository;
     private final RankingRepository rankingRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 인기 도시 조회
@@ -35,7 +43,52 @@ public class CityService {
                                                 cityUniqueDto.countryCode(),
                                                 cityUniqueDto.lat(),
                                                 cityUniqueDto.lng())
-            .orElseThrow(() -> new IllegalArgumentException("해당 도시를 찾을 수 없습니다."));
+            .orElseThrow(() -> new CityNotFoundException("해당 도시를 찾을 수 없습니다."));
         return city.getCityId();
+    }
+
+    // 멤버 여부는 그냥 받기만 할게요 일단은
+    public CityResponse addCity(Long memberId, CityRequest cityRequest) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 멤버입니다."));
+
+        return CityResponse.toResponse(cityRepository.save(cityRequest.toCity()));
+    }
+
+    @Transactional
+    public CityResponse updateCity(Long memberId, Long cityId, CityRequest cityRequest) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 멤버입니다."));
+
+        City city = cityRepository.findById(cityId)
+            .orElseThrow(() -> new IllegalArgumentException("해당 도시를 찾을 수 없습니다."));
+
+        return CityResponse.toResponse(
+            city.updateCity(
+                cityRequest.cityName(),
+                cityRequest.countryName(),
+                cityRequest.lat(),
+                cityRequest.lng(),
+                cityRequest.countryCode()
+            ));
+    }
+
+    @Transactional
+    public CityResponse deleteCity(Long memberId, Long cityId) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 멤버입니다."));
+
+        return cityRepository.findById(cityId)
+            .map(city -> {
+                cityRepository.delete(city);
+                return CityResponse.toResponse(city);
+            })
+            .orElseThrow(() -> new CityNotFoundException("해당 도시를 찾을 수 없습니다."));
+    }
+
+    public CityResponse findCityByCountryAndCityName(String countryCode, String cityName) {
+        return cityRepository.findByCountryCodeAndCityName(countryCode, cityName).map(CityResponse::toResponse)
+            .orElseThrow(() -> new CityNotFoundException("해당 도시를 찾을 수 없습니다."));
+
     }
 }
