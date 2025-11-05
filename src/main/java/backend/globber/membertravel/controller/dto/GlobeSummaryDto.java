ed028486b2709512fc9dc586acbfea8d1fbbf4cd
Minu Kim <kmw106933@naver.com>
2025-11-06 00:05:29 +0900

    [REFACTOR] 닉네임 추가

--- src/main/java/backend/globber/membertravel/controller/dto/GlobeSummaryDto.java
+++ src/main/java/backend/globber/membertravel/controller/dto/GlobeSummaryDto.java
@@ -3,6 +3,7 @@ package backend.globber.membertravel.controller.dto;
 import java.util.List;
 
 public record GlobeSummaryDto(
+        String nickname,
         int cityCount,
         int countryCount,
         List<RegionDto> regions
