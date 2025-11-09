ca85f366aba00f7fc77e2e1296b34c126271039c
Minu Kim <kmw106933@naver.com>
2025-11-10 00:13:47 +0900

    [REFACTOR] 대륙 나라 추가

--- src/main/java/backend/globber/membertravel/service/Continent.java
+++ src/main/java/backend/globber/membertravel/service/Continent.java
@@ -11,34 +11,34 @@ public enum Continent {
             "AFG", "ARM", "AZE", "BHR", "BGD", "BTN", "BRN", "KHM", "CHN", "CYP", "GEO", "HKG", "IND", "IDN",
             "IRN", "IRQ", "ISR", "JPN", "JOR", "KAZ", "KWT", "KGZ", "LAO", "LBN", "MAC", "MYS", "MDV", "MNG",
             "MMR", "NPL", "PRK", "OMN", "PAK", "PSE", "PHL", "QAT", "SAU", "SGP", "KOR", "LKA", "SYR", "TWN",
-            "TJK", "THA", "TLS", "TUR", "TKM", "ARE", "UZB", "VNM", "YEM"
+            "TJK", "THA", "TLS", "TUR", "TKM", "ARE", "UZB", "VNM", "YEM", "CCK", "CXR", "IOT"
     )),
     EUROPE("유럽", List.of(
             "ALB", "AND", "AUT", "BLR", "BEL", "BIH", "BGR", "HRV", "CZE", "DNK", "EST", "FRO", "FIN", "FRA",
             "DEU", "GIB", "GRC", "GGY", "HUN", "ISL", "IRL", "IMN", "ITA", "JEY", "LVA", "LIE", "LTU", "LUX",
             "MLT", "MDA", "MCO", "MNE", "NLD", "MKD", "NOR", "POL", "PRT", "ROU", "SMR", "SRB", "SVK", "SVN",
-            "ESP", "SWE", "CHE", "UKR", "GBR", "VAT"
+            "ESP", "SWE", "CHE", "UKR", "GBR", "VAT", "SJM", "XKX", "ALA"
     )),
     NORTH_AMERICA("북미", List.of(
             "AIA", "ATG", "BHS", "BRB", "BLZ", "BMU", "CAN", "CYM", "CRI", "CUB", "DMA", "DOM", "SLV", "GRD",
             "GLP", "GTM", "HTI", "HND", "JAM", "MTQ", "MEX", "MSR", "ANT", "CUW", "KNA", "LCA", "VCT", "SPM",
-            "TTO", "TCA", "USA", "VIR", "PRI", "GUF", "GRL", "BLM", "MAF"
+            "TTO", "TCA", "USA", "VIR", "PRI", "GUF", "GRL", "BLM", "MAF", "BES", "SXM"
     )),
     SOUTH_AMERICA("남미", List.of(
-            "ARG", "BOL", "BRA", "CHL", "COL", "ECU", "GUY", "PRY", "PER", "SUR", "URY", "VEN", "FLK"
+            "ARG", "BOL", "BRA", "CHL", "COL", "ECU", "GUY", "PRY", "PER", "SUR", "URY", "VEN", "FLK", "SGS"
     )),
     OCEANIA("오세아니아", List.of(
             "ASM", "AUS", "COK", "FJI", "PYF", "GUM", "KIR", "MHL", "FSM", "NRU", "NCL", "NZL", "NIU", "NFK",
-            "MNP", "PLW", "PNG", "WSM", "SLB", "TKL", "TON", "TUV", "VUT", "WLF"
+            "MNP", "PLW", "PNG", "WSM", "SLB", "TKL", "TON", "TUV", "VUT", "WLF", "PCN"
     )),
     AFRICA("아프리카", List.of(
             "DZA", "AGO", "BEN", "BWA", "BFA", "BDI", "CMR", "CPV", "CAF", "TCD", "COM", "COG", "COD", "DJI",
             "EGY", "GNQ", "ERI", "SWZ", "ETH", "GAB", "GMB", "GHA", "GIN", "GNB", "CIV", "KEN", "LSO", "LBR",
             "LBY", "MDG", "MWI", "MLI", "MRT", "MUS", "MYT", "MAR", "MOZ", "NAM", "NER", "NGA", "REU", "RWA",
-            "STP", "SEN", "SYC", "SLE", "SOM", "ZAF", "SSD", "SDN", "TZA", "TGO", "TUN", "UGA", "ZMB", "ZWE"
+            "STP", "SEN", "SYC", "SLE", "SOM", "ZAF", "SSD", "SDN", "TZA", "TGO", "TUN", "UGA", "ZMB", "ZWE", "SHN"
     )),
     ANTARCTICA("남극", List.of(
-            "ATA", "BVT", "HMD", "SGS", "ATF"
+            "ATA", "BVT", "HMD", "ATF"
     )),
     OTHER("기타", List.of(
             "ESH"
