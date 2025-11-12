19b16c70177f222078c2b4b05180a309ede488e2
Minu Kim <kmw106933@naver.com>
2025-11-12 22:48:04 +0900

    [FIX] 회원 탈퇴시 이메일 변경 실패 버그 수정

--- src/main/java/backend/globber/auth/domain/Member.java
+++ src/main/java/backend/globber/auth/domain/Member.java
@@ -45,7 +45,7 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 100)
     private String email;
     @Column(nullable = false, length = 30)
     private String name;
